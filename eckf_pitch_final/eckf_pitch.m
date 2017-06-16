7daaa3b47f92d397ff020b8c0e18c9b1943f0333
orchisama <orchisama.das@gmail.com>
2017-06-16 00:19:19 -0700

    Changes according to final version of paper

--- eckf_pitch_final/eckf_pitch.m
+++ eckf_pitch_final/eckf_pitch.m
@@ -1,4 +1,4 @@
-function [f0,amp,phi1,x_est] = eckf_pitch(y,fs,c, numBufToWait)
+function [f0,amp,phase,x_est] = eckf_pitch(y,fs,c, numBufToWait)
 
 %pitch detector based on the extended complex kalman filter
 %y - incoming noisy signal (row vector) 
@@ -24,7 +24,7 @@ nframes = ceil(length(y)/flength);
 y = [y, zeros(1,nframes*flength - length(y))];
 
 Ts = 1/fs;
-H =[0,0.5,-0.5];
+H =[0,0.5,0.5];
 
 %Kalman filter variables
 K = zeros(3,1);
@@ -44,7 +44,6 @@ while(start + flength - 1 < length(y))
     y_frame = y(start:start+flength-1);
     prev_frame = cur_frame;
     cur_frame = is_silent(y_frame);
-    silent_inds = 1;
     
     %if current frame is silent, then continue
     if(cur_frame == 1)
@@ -75,20 +74,10 @@ while(start + flength - 1 < length(y))
          
     %reset covariance matrix and calculate initial states
     if(flag == 1)
-        silent_inds = amp_follower(y_frame, 1);
-        %if there are very few samples remaining in the rest of the frame,
-        %then we should mark the frame as silent and proceed to the next frame
-        if(flength - silent_inds(end) < (fs/100))
-            start = start + flength;
-            n = start;
-            flag = 0;
-            cur_frame = 1;
-            continue;
-        end 
-	
+
         %calculate initial state by taking an FFT and detecting the first peak
-        n = n + silent_inds(end) + 1;
-        ybuf = y_frame(silent_inds(end):end);
+        n = n + 1;
+        ybuf = y_frame;
         minf = length(ybuf);
         nfft = 2^nextpow2(4*minf);
         fbins = linspace(-fs/2,fs/2,nfft+1);
@@ -101,7 +90,7 @@ while(start + flength - 1 < length(y))
         Y = Y(nfft/2 + nbins_below50:end); 
         fbins = fbins(nfft/2 + 1 + nbins_below50:end);
         mag = abs(Y)./mean(win);
-        phase = angle(Y);
+        phi = angle(Y);
         %take the least frequency peak to be fundamental, ignore harmonics
         [m, mpos] = findpeaks(mag);
         [val, ind] = sort(m, 'descend');
@@ -110,21 +99,20 @@ while(start + flength - 1 < length(y))
         [a1,ppos] = parabolic_interpolation(mag(mpos-1),mag(mpos),mag(mpos+1));
         a1 = a1/nfft;
         f1 = fbins(mpos) + (ppos * fs/nfft);
-        [phi1,pos] = parabolic_interpolation(phase(mpos-1),phase(mpos),phase(mpos+1));
+        [phi1,pos] = parabolic_interpolation(phi(mpos-1),phi(mpos),phi(mpos+1));
         x0 = [exp(1i*2*pi*f1*Ts);a1*exp(1i*2*pi*f1*n*Ts + 1i*phi1);...
             a1*exp(-1i*2*pi*f1*n*Ts - 1i*phi1)];
         P0 = 0;
-        %P0 = abs(y_frame(silent_inds(end)+1) - H*x0);
         
         %uncomment to plot estimated states
-        figure;
-        set(gca, 'fontsize', 14);
-        hold on
-        subplot(211);plot(fbins, mag);grid on;hold on;
-        plot(f1,a1*nfft,'k*');hold off;
-        ylabel('Magnitude spectrum');xlabel('Frequency in Hz');
-        subplot(212);plot(fbins,unwrap(phase));grid on;   
-        ylabel('Phase spectrum');xlabel('Frequency in Hz');
+%         figure;
+%         set(gca, 'fontsize', 14);
+%         hold on
+%         subplot(211);plot(fbins, mag);grid on;hold on;
+%         plot(f1,a1*nfft,'k*');hold off;
+%         ylabel('Magnitude spectrum');xlabel('Frequency in Hz');
+%         subplot(212);plot(fbins,unwrap(phase));grid on;   
+%         ylabel('Phase spectrum');xlabel('Frequency in Hz');
 
         %reset covariance matrix
         if(abs(min(K)) < Kthres)
@@ -135,7 +123,7 @@ while(start + flength - 1 < length(y))
         end
     end
     
-    for k = silent_inds(end)+1:flength
+    for k = 1:flength
         %ekf equations
         K = (P_last*H')/(H*P_last*H' + 1);
         P = P_last - K*H*P_last;
@@ -147,7 +135,7 @@ while(start + flength - 1 < length(y))
         P_next = F*P*F' + Q(n)*eye(3);
         f0(n) = abs(log(x(1))/(1j*Ts*2*pi));
         amp(n) = abs(x(2));
-        phase(n) = -1i * (log(x(2)/amp(n))-(2*pi*(1i)*f0(n)*Ts*n));
+        phase(n) = abs(-1i * (log(x(2)/amp(n))-(2*pi*f0(n)*Ts*n)));
         x_est(n) = H*x;
 
         P_last = P_next;
